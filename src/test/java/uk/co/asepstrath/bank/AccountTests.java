99dcd09f12e61a88fdbfca9a47d25537c9fb5c18
seanm <sean.mcilroy.2022@uni.strath.ac.uk>
2024-01-25 14:48:54 +0000

    Test 5 added, Comments added

--- src/test/java/uk/co/asepstrath/bank/AccountTests.java
+++ src/test/java/uk/co/asepstrath/bank/AccountTests.java
@@ -11,13 +11,13 @@ public class AccountTests {
         Assertions.assertNotNull(a);
     }
 
-    @Test
+    @Test //Test 1
     public void newAccount(){
         Account a = new Account();
         Assertions.assertEquals(0, a.getBalance());
     }
 
-    @Test
+    @Test //Test 2
     public void addFunds(){
         Account a = new Account();
         a.deposit(20);
@@ -26,7 +26,7 @@ public class AccountTests {
         Assertions.assertEquals(70, a.getBalance());
     }
 
-    @Test
+    @Test //Test 3
     public void withdraw(){
         Account a = new Account();
         a.deposit(40);
@@ -34,10 +34,25 @@ public class AccountTests {
         Assertions.assertEquals(20, a.getBalance());
     }
 
-    @Test
+    @Test //Test 4
     public void overdraft(){
         Account a = new Account();
-        a.deposit(30);
+        a.deposit(30); //Starting balance
         Assertions.assertFalse(a.withdraw(100));
+    }
+
+    @Test //Test 5
+    public void mulDeposit(){
+        Account a = new Account();
+        a.deposit(20); //Starting balance
+        a.deposit(10);
+        a.deposit(10);
+        a.deposit(10);
+        a.deposit(10);
+        a.deposit(10);
+        a.withdraw(20);
+        a.withdraw(20);
+        a.withdraw(20);
+        Assertions.assertEquals(10,a.getBalance());
     }
 }
