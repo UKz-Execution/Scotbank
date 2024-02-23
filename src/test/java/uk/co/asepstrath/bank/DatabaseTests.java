c101b1b548ffa3aa41d24012472813493ed380e4
zakbm <zak.mcgill.2022@uni.strath.ac.uk>
2024-02-23 19:12:43 +0000

    Added tests for updating accounts

--- src/test/java/uk/co/asepstrath/bank/DatabaseTests.java
+++ src/test/java/uk/co/asepstrath/bank/DatabaseTests.java
@@ -4,24 +4,38 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.rules.ExpectedException;
+import uk.co.asepstrath.bank.accounts.Account;
 import uk.co.asepstrath.bank.database.DatabaseAPI;
 
 import javax.sql.DataSource;
 import io.jooby.Jooby;
 import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.UUID;
 
 public class DatabaseTests extends Jooby{
-    DatabaseAPI d;
-    @BeforeEach
+    private DatabaseAPI db;
+    private final DataSource ds = require(DataSource.class);
+    private final UUID id = UUID.randomUUID();
+    @Test
     void initialise() throws Exception { // Check database connection and creation
-        DataSource db = require(DataSource.class);
-        Assertions.assertNotNull(db);
-        DatabaseAPI.initDatabase(db);
+        DatabaseAPI.initDatabase(ds);
+        Assertions.assertNotNull(ds);
     }
 
     @Test
-    void createTable() {
+    void testUpdateName() throws SQLException { // Check name updates correctly via id
+        db.updateAccountName(id, "John Smith");
+        Account a = db.getAccountById(id);
+        Assertions.assertEquals(id, a.id);
+        Assertions.assertEquals("John Smith", a.getName());
+    }
 
+    @Test
+    void testUpdateBalance() throws SQLException {
+        db.updateAccountBalance(id, BigDecimal.valueOf(50.0));
+        Account a  = db.getAccountById(id);
+        Assertions.assertEquals(BigDecimal.valueOf(50.0), a.getBalance());
     }
 }
