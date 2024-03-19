1795e8747eafca81c06e545aced8fd6ee0e00a25
seanm <sean.mcilroy.2022@uni.strath.ac.uk>
2024-03-19 17:07:56 +0000

    Calculate current amount method added after transactions, Test completed.

--- src/test/java/uk/co/asepstrath/bank/ControllerTests.java
+++ src/test/java/uk/co/asepstrath/bank/ControllerTests.java
@@ -3,7 +3,15 @@ package uk.co.asepstrath.bank;
 import io.jooby.Context;
 import io.jooby.test.MockContext;
 import org.junit.jupiter.api.Test;
+import uk.co.asepstrath.bank.transactions.Transaction;
 import uk.co.asepstrath.bank.webcontrollers.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class ControllerTests {
 
@@ -58,5 +66,23 @@ public class ControllerTests {
         WebController controller = new WebController();
 
 //        Assertions.assertEquals();
+    }
+
+    @Test
+    public void calcCurrentAmount(){
+        double startingAmount = 100.0;
+        String accountUUID = "account123";
+
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        transactions.add(new Transaction(LocalDateTime.now(), UUID.randomUUID(), "PAYMENT", BigDecimal.valueOf(50.0), null, "account123"));
+        transactions.add(new Transaction(LocalDateTime.now(), UUID.randomUUID(), "DEPOSIT", BigDecimal.valueOf(30.0), "account123", null));
+        transactions.add(new Transaction(LocalDateTime.now(), UUID.randomUUID(), "WITHDRAWAL", BigDecimal.valueOf(20.0), "receiver", "account123"));
+        transactions.add(new Transaction(LocalDateTime.now(), UUID.randomUUID(), "TRANSFER", BigDecimal.valueOf(10.0), null, "account123"));
+
+        BigDecimal expectedAmount = BigDecimal.valueOf(100.0).subtract(BigDecimal.valueOf(50.0)).add(BigDecimal.valueOf(30.0)).subtract(BigDecimal.valueOf(20.0)).subtract(BigDecimal.valueOf(10.0));
+        BigDecimal actualAmount = new AccountController().calculateCurrentAmount(startingAmount, transactions, accountUUID);
+
+        assertEquals(expectedAmount, actualAmount);
+
     }
 }
